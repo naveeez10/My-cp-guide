fe3522d88360de00a2c1e708f329d37cee01019b
Naviz Khoja <naviz.khoja@eventbeep.com>
2023-08-01 18:37:13 +0530

    added fenwick tree

--- Template.cpp
+++ Template.cpp
@@ -1,71 +1,169 @@
-#include"bits/stdc++.h"
+#include "bits/stdc++.h"
 using namespace std;
 const int mod = 1e9 + 7;
 #define ll long long
 #define ld long double
 #define f first
 #define s second
-#define endl '\n'
 #define pb push_back
 #define MP make_pair
 #define INF LLONG_MAX
-#define vi vector<int>
-#define vll vector<ll>
-#define pl pair<ll,ll>
-#define pi pair<int,int>
-#define mint map<int,int>
-#define llmint map<ll,ll>
 #define nline cout << nl;
 #define sz(x) (int)x.size()
-#define all(x) x.begin(),x.end()
-#define rall(x) x.rbegin(),x.rend()
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
 #define spree(x) fixed << setprecision(x)
 
 #ifndef ONLINE_JUDGE
-#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
-#define debug2(x,y) cerr << #x <<" "; _print(x); cerr << sp; cerr << #y <<" "; _print(y); cerr << endl;
-#define debug3(x,y,z) cerr << #x <<" "; _print(x); cerr << sp;cerr << #y <<" "; _print(y); cerr << sp;cerr << #z <<" "; _print(z); cerr << nl;
+#define debug(x)         \
+      cerr << #x << " "; \
+      _print(x);         \
+      cerr << endl;
+#define debug2(x, y)     \
+      cerr << #x << " "; \
+      _print(x);         \
+      cerr << sp;        \
+      cerr << #y << " "; \
+      _print(y);         \
+      cerr << endl;
+#define debug3(x, y, z)  \
+      cerr << #x << " "; \
+      _print(x);         \
+      cerr << sp;        \
+      cerr << #y << " "; \
+      _print(y);         \
+      cerr << sp;        \
+      cerr << #z << " "; \
+      _print(z);         \
+      cerr << nl;
 #else
 #define debug(x)
-#define debug2(x,y)
-#define debug3(x,y,z)
+#define debug2(x, y)
+#define debug3(x, y, z)
 #endif
 
-void _print(ll t) {cerr << t;}
-void _print(int t) {cerr << t;}
-void _print(string t) {cerr << t;}
-void _print(char t) {cerr << t;}
-void _print(ld t) {cerr << t;}
-void _print(double t) {cerr << t;}
-
-template <class T, class V> void _print(pair <T, V> p);
-template <class T> void _print(vector <T> v);
-template <class T> void _print(set <T> v);
-template <class T> void _print(deque <T> v);
-template <class T> void _print(priority_queue <T> v);
-template <class T, class V> void _print(map <T, V> v);
-template <class T> void _print(multiset <T> v);
-template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.f); cerr << ","; _print(p.s); cerr << "}";}
-template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T, class V> void _print(unordered_map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(deque <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(priority_queue <T> v) {cerr << "[ "; while (!v.empty()) {_print(v.top()); v.pop(); cerr << " ";} cerr << "]";}
-template <class T> void _print(priority_queue <T, vector<T>, greater<T>> v) {cerr << "[ "; while (!v.empty()) {_print(v.top()); v.pop(); cerr << " ";} cerr << "]";}
-
-void yesret() {
-      cout << "YES" << nl;
+void _print(ll t)
+{
+      cerr << t;
 }
-void noret() {
-      cout << "NO" << nl;
-}
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(ld t) { cerr << t; }
+void _print(double t) { cerr << t; }
 
-void read(vector<int> &a) {
-      for (auto &x : a) {
-            cin >> x;
+template <class T, class V>
+void _print(pair<T, V> p);
+template <class T>
+void _print(vector<T> v);
+template <class T>
+void _print(set<T> v);
+template <class T>
+void _print(deque<T> v);
+template <class T>
+void _print(priority_queue<T> v);
+template <class T, class V>
+void _print(map<T, V> v);
+template <class T>
+void _print(multiset<T> v);
+template <class T, class V>
+void _print(pair<T, V> p)
+{
+      cerr << "{";
+      _print(p.f);
+      cerr << ",";
+      _print(p.s);
+      cerr << "}";
+}
+template <class T>
+void _print(vector<T> v)
+{
+      cerr << "[ ";
+      for (T i : v)
+      {
+            _print(i);
+            cerr << " ";
       }
+      cerr << "]";
+}
+template <class T>
+void _print(set<T> v)
+{
+      cerr << "[ ";
+      for (T i : v)
+      {
+            _print(i);
+            cerr << " ";
+      }
+      cerr << "]";
+}
+template <class T>
+void _print(multiset<T> v)
+{
+      cerr << "[ ";
+      for (T i : v)
+      {
+            _print(i);
+            cerr << " ";
+      }
+      cerr << "]";
+}
+template <class T, class V>
+void _print(map<T, V> v)
+{
+      cerr << "[ ";
+      for (auto i : v)
+      {
+            _print(i);
+            cerr << " ";
+      }
+      cerr << "]";
+}
+template <class T, class V>
+void _print(unordered_map<T, V> v)
+{
+      cerr << "[ ";
+      for (auto i : v)
+      {
+            _print(i);
+            cerr << " ";
+      }
+      cerr << "]";
+}
+template <class T>
+void _print(deque<T> v)
+{
+      cerr << "[ ";
+      for (T i : v)
+      {
+            _print(i);
+            cerr << " ";
+      }
+      cerr << "]";
+}
+template <class T>
+void _print(priority_queue<T> v)
+{
+      cerr << "[ ";
+      while (!v.empty())
+      {
+            _print(v.top());
+            v.pop();
+            cerr << " ";
+      }
+      cerr << "]";
+}
+template <class T>
+void _print(priority_queue<T, vector<T>, greater<T>> v)
+{
+      cerr << "[ ";
+      while (!v.empty())
+      {
+            _print(v.top());
+            v.pop();
+            cerr << " ";
+      }
+      cerr << "]";
 }
 
 ////////////////////////////////////////////////////////////////////
@@ -78,11 +176,12 @@ const char nl = '\n';
 const int N = 1e5 + 5;
 const int inf = 1e9 + 69;
 
-void getans() {
-
+void getans()
+{
 }
 
-int main() {
+int main()
+{
 
 #ifndef ONLINE_JUDGE
       freopen("Error.txt", "w", stderr);
@@ -93,12 +192,15 @@ int main() {
 
       ll T = 1;
 
-      if (testcase) {
+      if (testcase)
+      {
             cin >> T;
       }
 
-      while (T--) {
-            if (cases) {
+      while (T--)
+      {
+            if (cases)
+            {
                   cout << "Case #" << casenumber << ": ";
                   casenumber++;
             }
